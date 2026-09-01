983d628434e361eebb8f43de8ae042da1b869144
Opeyemi <opeyemioluwadamilare415@gmail.com>
2026-09-01 19:01:47 +0100

    Fixed Firebase config for deployment

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -1,16 +1,20 @@
-const admin = require('firebase-admin');
+const { initializeApp, cert } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
 
 let serviceAccount;
+
+// If running on Render, use the Environment Variable
 if (process.env.FIREBASE_SERVICE_ACCOUNT) {
   serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 } else {
+  // If running locally, use the local file
   serviceAccount = require('../serviceAccountKey.json');
 }
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+const app = initializeApp({
+  credential: cert(serviceAccount)
 });
 
-const db = admin.firestore();
+const db = getFirestore(app);
 
-module.exports = { admin, db };
+module.exports = { db };
